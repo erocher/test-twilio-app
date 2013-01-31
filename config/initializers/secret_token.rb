e7d7a1c4dc53e179ff36b2822de4c82aa70ea467
Evan Rocher <evanrocher@gmail.com>
2013-01-31 15:03:20 -0600

    here we go again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwilioCaller::Application.config.secret_token = 'a404a3ef8a310163b387c7482f8ac6412e823bcfbc2a85085ff78f5920008b75c5601884acad78e1eaddb193edd1202504a08614e223085b6f2132187d348958'
